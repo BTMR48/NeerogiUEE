33cf6e3ef114b4e73e9e78f12f5e27f3bd1a5f37
dasun <tharindudasun0@gmail.com>
2022-11-05 23:52:15 +0530

    finish add questions article

---
+++ lib/utils/config.dart
@@ -0,0 +1,12 @@
+class Config {
+  static const app_background = 'images/background1.jpg';
+
+  //Screen test questions
+  static const app_Screentestquestion1 = 'ළමයා දිනකට කොච්චර වචන පවසනවා ද?';
+  static const app_Screentestquestion2 =
+      'ඔබ කාමරය හරහ යමක් පෙන්නු විට, දරුවා ඒ දෙස බලනවා ද?';
+  static const app_Screentestquestion3 =
+      'ඔබේ දරුවාට යමක් පෙන්නු විට ඒය හදුනා ගනන්වාද?';
+  static const app_Screentestquestion4 = 'ඔබේ දරුවා වර්ණ 5කට වඩා හදුනනවාද?';
+  static const app_Screentestquestion5 = 'ඔබේ දරුවා පලමු වචනය පැවසුවේ?';
+}
