4128a2dc57f0f262b5b690eae059ca97c6ca6b2f
dasun <tharindudasun0@gmail.com>
2022-11-06 00:06:17 +0530

    finish add questions article

--- lib/screens/ScreenTest/marks_screen.dart
+++ lib/screens/ScreenTest/marks_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import '../../utils/config.dart';
 
@@ -15,6 +16,7 @@ class MarksScreen extends StatefulWidget {
 class _MarksScreenState extends State<MarksScreen> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     print(widget.marks);
     return Scaffold(
       body: Container(
@@ -41,11 +43,97 @@ class _MarksScreenState extends State<MarksScreen> {
                   ),
                 ],
               ),
-              Card()
+              Card(
+                margin: const EdgeInsets.only(top: 0, right: 10, left: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(28.0),
+                ),
+                shadowColor: Colors.white,
+                color: HexColor('#FFFBFB').withOpacity(0.7),
+                child: SizedBox(
+                  height: height * 0.82,
+                  width: (MediaQuery.of(context).size.width),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 5, right: 20, left: 0),
+                    child: Column(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 5,
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical, //.horizontal
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 10, right: 10, left: 10),
+                              child: Column(
+                                children: <Widget>[
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      'නිරෝගී',
+                                      style: TextStyle(
+                                        color: HexColor('#1F1010'),
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 30),
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        Text(
+                                          'මුලු ලකුණූ',
+                                          style: TextStyle(
+                                            color: HexColor('#1F1010'),
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 50,
+                                          width: 100,
+                                          child: Card(
+                                            color: Colors.greenAccent,
+                                            child: Center(
+                                              child: Text(
+                                                widget.marks.toString(),
+                                                style: TextStyle(
+                                                  color: HexColor('#1F1010'),
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          child: result(),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  result() {
+    if (widget.marks >= 3) {}
   }
 }
